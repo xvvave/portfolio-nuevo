80858f731ff1de15071219aba882f6ceffa88c9e
Lautaro Romero Temperini <latta.romero@gmail.com>
2025-07-09 08:50:05 -0300

    Redeploy 7 - vorterix

--- src/app/playground/Playground.tsx
+++ src/app/playground/Playground.tsx
@@ -6,7 +6,6 @@ import OptimizedImage from "../../components/optimized-image"
 import { playgroundItems } from "@/data/playgroundItems"
 import { filterOptions } from "@/data/playgroundFilters"
 import GlareHover from "../../components/fxscripts/GlareHover"
-import ShinyText from "../../components/fxscripts/ShinyText"
 
 export default function PlaygroundGrid() {
   const [activeFilter, setActiveFilter] = useState("all")
